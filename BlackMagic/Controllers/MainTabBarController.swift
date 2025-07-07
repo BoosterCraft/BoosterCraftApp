af502d7e122de7850c56864da6adb8e9ab189a88
Alex Sergeev <88105767+alwwwd@users.noreply.github.com>
2025-07-07 16:10:16 +0300

    фиксы

--- BlackMagic/Controllers/MainTabBarController.swift
+++ BlackMagic/Controllers/MainTabBarController.swift
@@ -7,18 +7,41 @@ class MainTabBarController: UITabBarController {
     override func viewDidLoad() {
         super.viewDidLoad()
         setupTabs()
+        
         tabBar.tintColor = .systemBlue
         tabBar.barStyle = .black
         tabBar.isTranslucent = true
     }
+    override func viewDidAppear(_ animated: Bool) {
+            super.viewDidAppear(animated)
+            showOnboardingIfNeeded()
+        }
+
+    private func showOnboardingIfNeeded() {
+        // Проверяем, был ли онбординг уже пройден
+//        guard !UserDefaults.standard.bool(forKey: "hasCompletedOnboarding") else {
+//            return
+//        }
+        let title: NSString = "Welcome to Black Magic"
+        let detailText: NSString = "An example of how to use OnBoardingKit."
+        let welcomeController = OBWelcomeController(title: title, detailText: detailText, symbolName: nil)
+        welcomeController.addBulletedListItem(title: "MTG boosters here", description: "Virtual versions of Magic: the Gathering packs available", symbolName: "sparkles.rectangle.stack")
+        welcomeController.addBulletedListItem(title: "Buy and open packs for free", description: "Get virtual currency every day.", symbolName: "infinity")
+        welcomeController.addBulletedListItem(title: "Sell and trade your cards", description: "Exchange them for currency or make deals with players", symbolName: "arrow.left.arrow.right")
+        
+        welcomeController.addBoldButton(title: "Get Started") { self.dismiss(animated: true) }
+        welcomeController.addLinkButton(title: "Not Now") { self.dismiss(animated: true) }
+        
+        present(welcomeController.viewController, animated: true)
     
+
+    }
     // MARK: - Setup Tabs
     
     private func setupTabs() {
         let mainVC = MainViewController()
         let boostersVC = OpenBoostersViewController()
-        let collectionVC = UIViewController()
-        collectionVC.view.backgroundColor = .black
+        let collectionVC = MyCollectionViewController()
         
         mainVC.tabBarItem = UITabBarItem(title: "Buy boosters", image: UIImage(systemName: "cart.fill"), tag: 0)
         boostersVC.tabBarItem = UITabBarItem(title: "Open boosters", image: UIImage(systemName: "gift.fill"), tag: 1)
