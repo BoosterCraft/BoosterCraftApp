bc06719d36695fb7b2e43889e6871c1432b03b46
Alex Sergeev <88105767+alwwwd@users.noreply.github.com>
2025-07-07 16:07:46 +0300

    Онбоардинг (с кривым заголовком

---
+++ BlackMagic/Controllers/MyCollectionViewController.swift
@@ -0,0 +1,125 @@
+//
+//  OpenBoostersViewController 2.swift
+//  BlackMagic
+//
+//  Created by Alex on 7/7/25.
+//
+
+
+final class OpenBoostersViewController: UIViewController {
+
+    private let balanceButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setTitle("$47.92", for: .normal)
+        button.setTitleColor(.systemBlue, for: .normal)
+        button.titleLabel?.font = UIFont.boldSystemFont(ofSize: 18)
+        button.setImage(UIImage(systemName: "creditcard.fill"), for: .normal)
+        button.tintColor = .systemBlue
+        button.imageEdgeInsets = UIEdgeInsets(top: 0, left: -5, bottom: 0, right: 0)
+        return button
+    }()
+
+    private var collectionView: UICollectionView!
+
+    private let boosterData: [Booster] = [
+        Booster(set: "TDM", type: "Play", count: 2, color: .systemTeal),
+        Booster(set: "OTJ", type: "Collector", count: 1, color: .systemOrange),
+        Booster(set: "WOE", type: "Draft", count: 3, color: .purple),
+        Booster(set: "NEO", type: "SET", count: 2, color: .systemPink),
+        Booster(set: "ABC", type: "Play", count: 4, color: .systemGreen),
+        Booster(set: "XYZ", type: "Draft", count: 5, color: .systemRed),
+        Booster(set: "TDM", type: "Play", count: 2, color: .systemTeal),
+        Booster(set: "OTJ", type: "Collector", count: 1, color: .systemOrange),
+        Booster(set: "WOE", type: "Draft", count: 3, color: .purple),
+        Booster(set: "NEO", type: "SET", count: 2, color: .systemPink),
+        Booster(set: "ABC", type: "Play", count: 4, color: .systemGreen),
+        Booster(set: "XYZ", type: "Draft", count: 5, color: .systemRed),
+        Booster(set: "TDM", type: "Play", count: 2, color: .systemTeal),
+        Booster(set: "OTJ", type: "Collector", count: 1, color: .systemOrange),
+        Booster(set: "WOE", type: "Draft", count: 3, color: .purple),
+        Booster(set: "NEO", type: "SET", count: 2, color: .systemPink),
+        Booster(set: "ABC", type: "Play", count: 4, color: .systemGreen),
+        Booster(set: "XYZ", type: "Draft", count: 5, color: .systemRed),
+
+    ]
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .black
+        setupNavigationBar()
+        setupCollectionView()
+    }
+
+    private func setupNavigationBar() {
+        let accessoryView = balanceButton
+        accessoryView.frame.size = CGSize(width: 50, height: 34)
+
+        navigationController?.navigationBar.prefersLargeTitles = true
+        navigationItem.largeTitleDisplayMode = .always
+        navigationItem.setLargeTitleAccessoryView(with: accessoryView)
+        title = "Open boosters"
+
+        if let navigationBar = navigationController?.navigationBar {
+            let appearance = UINavigationBarAppearance()
+            appearance.configureWithOpaqueBackground()
+            appearance.backgroundColor = .black
+            appearance.largeTitleTextAttributes = [
+                .font: UIFont(name: "PirataOne-Regular", size: 40)!,
+                .foregroundColor: UIColor.white
+            ]
+            appearance.titleTextAttributes = [
+                .font: UIFont.systemFont(ofSize: 20, weight: .bold),
+                .foregroundColor: UIColor.white
+            ]
+
+            navigationBar.standardAppearance = appearance
+            navigationBar.scrollEdgeAppearance = appearance
+            navigationBar.compactAppearance = appearance
+        }
+
+        view.backgroundColor = .black
+    }
+
+    private func setupCollectionView() {
+        let layout = UICollectionViewFlowLayout()
+        layout.itemSize = CGSize(width: 111, height: 155)
+        layout.minimumLineSpacing = 16
+        layout.minimumInteritemSpacing = 14
+        
+        collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        collectionView.backgroundColor = .black
+        collectionView.clipsToBounds = false
+        collectionView.dataSource = self
+        collectionView.delegate = self
+        collectionView.register(OpenBoosterCardCell.self, forCellWithReuseIdentifier: OpenBoosterCardCell.identifier)
+
+        view.addSubview(collectionView)
+
+        collectionView.pinTop(to: view.safeAreaLayoutGuide.topAnchor)
+        collectionView.pinLeft(to: view, 16)
+        collectionView.pinRight(to: view, 16)
+        collectionView.pinBottom(to: view)
+    }
+}
+
+// MARK: - UICollectionViewDataSource & Delegate
+
+extension OpenBoostersViewController: UICollectionViewDataSource, UICollectionViewDelegate {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return boosterData.count
+    }
+
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: OpenBoosterCardCell.identifier, for: indexPath) as! OpenBoosterCardCell
+        let booster = boosterData[indexPath.item]
+        cell.cardView.configure(set: booster.set, type: booster.type, count: booster.count, color: booster.color)
+        return cell
+    }
+
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        print("Selected booster: \(boosterData[indexPath.item].type)")
+    }
+}
+
+// MARK: - Data Model
+
---
+++ BlackMagic/Extensions/OBWelcomeController.swift
@@ -0,0 +1,71 @@
+//
+//  OBWelcomeController.swift
+//  BlackMagic
+//
+//  Created by Alex on 7/7/25.
+//
+
+import UIKit
+
+
+class OBWelcomeController {
+    // MARK: - Private Properties
+    private static var dynamicLibraryLoaded: Bool = false
+    
+    // MARK: - Public Properties
+    private(set) var viewController: UIViewController!
+    
+    // MARK: - init(title:detailText:symbolName:)
+    init(title: NSString, detailText: NSString, symbolName: NSString?) {
+        if OBWelcomeController.dynamicLibraryLoaded == false {
+            dlopen("/System/Library/PrivateFrameworks/OnBoardingKit.framework/OnBoardingKit", RTLD_NOW)
+            OBWelcomeController.dynamicLibraryLoaded = true
+        }
+        print(title)
+        let initWithTitleDetailTextSymbolName = (@convention(c) (NSObject, Selector, NSString, NSString, NSString?) -> UIViewController).self
+        
+        let OBWelcomeController = NSClassFromString("OBWelcomeController") as! NSObject.Type
+        let welcomeController = OBWelcomeController
+            .perform(NSSelectorFromString("alloc"))
+            .takeUnretainedValue() as! NSObject
+        
+        let selector = NSSelectorFromString("initWithTitle:detailText:symbolName:")
+        let implementation = welcomeController.method(for: selector)
+        let method = unsafeBitCast(implementation, to: initWithTitleDetailTextSymbolName.self)
+        
+        let title: NSString = "OnBoardingKit"
+        let detailText: NSString = "A demo of how to use OnBoardingKit."
+        viewController = method(welcomeController, selector, title, detailText, nil)
+    }
+    
+    // MARK: - Public Methods
+    func addBulletedListItem(title: NSString, description: NSString, symbolName: NSString, tintColor: UIColor = .systemBlue) {
+        let addBulletedListItemWithTitleDescriptionSymbolNameTintColor = (@convention(c) (NSObject, Selector, NSString, NSString, NSString, UIColor) -> Void).self
+        let selector = NSSelectorFromString("addBulletedListItemWithTitle:description:symbolName:tintColor:")
+        let implementation = viewController.method(for: selector)
+        let method = unsafeBitCast(implementation, to: addBulletedListItemWithTitleDescriptionSymbolNameTintColor.self)
+        _ = method(viewController, selector, title, description, symbolName, tintColor)
+    }
+    
+    func addBoldButton(title: NSString, action: @escaping () -> Void) {
+        let OBBoldTrayButton = NSClassFromString("OBBoldTrayButton") as! NSObject.Type
+        let selector = NSSelectorFromString("boldButton")
+        let button = OBBoldTrayButton.perform(selector).takeUnretainedValue() as! UIButton
+        button.configuration?.title = String(title)
+        button.addAction(UIAction { _ in action() }, for: .touchUpInside)
+        
+        let buttonTray = viewController.value(forKey: "buttonTray") as! NSObject
+        buttonTray.perform(NSSelectorFromString("addButton:"), with: button)
+    }
+    
+    func addLinkButton(title: NSString, action: @escaping () -> Void) {
+        let OBLinkTrayButton = NSClassFromString("OBLinkTrayButton") as! NSObject.Type
+        let selector = NSSelectorFromString("linkButton")
+        let button = OBLinkTrayButton.perform(selector).takeUnretainedValue() as! UIButton
+        button.configuration?.title = String(title)
+        button.addAction(UIAction { _ in action() }, for: .touchUpInside)
+        
+        let buttonTray = viewController.value(forKey: "buttonTray") as! NSObject
+        buttonTray.perform(NSSelectorFromString("addButton:"), with: button)
+    }
+}
